d53cdfe0d763d41912a7954284e5cfdb8fa25a13
Hassan-Naveed1 <hassann4242@gmail.com>
2025-12-06 00:18:27 +0000

    Shopping Cart Log
    Shopping Cart Log Python with logs

--- online_shopping_cart.py
+++ online_shopping_cart.py
@@ -19,6 +19,20 @@ shopping_handler.setFormatter(formatter)
 #Assigning the handler to the logger
 shopping_cart.addHandler(shopping_handler)
 
-
-
-
+print("Items in Cart: ")
+products = {
+    "A":{"name":"Apple","price":2},
+    "B":{"name":"Banana","price":3},
+    "C":{"name":"Mango","price":4}
+}
+try:
+    choice = input("Please choose one of the above to add in cart: ")
+    if choice in products.keys():
+        shopping_cart.info(f"The entered {choice} exists in the cart")
+        quantity = int(input(f"Please enter the quantity of {choice} you would like to have: "))
+        final_quantity = quantity * products[choice]["price"]
+        shopping_cart.info(f"{quantity} {choice} has been added to the cart and the price of the product is: {final_quantity}")
+    if choice not in products:
+        raise ValueError("Invalid Product: The entered product doesnt exist.")
+except ValueError as e:
+    shopping_cart.info(f"Exception occured: {e}")
